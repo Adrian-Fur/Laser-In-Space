3761e196295906807c9054e12e35ee38cb4c7117
Adrian Furmanek <adrian.furm@gmail.com>
2021-05-26 23:29:53 +0200

    destroy enemy and wave looping
    2nd enemy, more patching, wave looping option, enemy destroy on laser hit(laser damage and enemy health)

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
-    List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
+    WaveConfig waveConfig;
+    List<Transform> waypoints;  
     int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
@@ -21,21 +20,28 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
             if (transform.position == targetPosition)
             {
                 waypointIndex++;
             }
-            else if(waypointIndex == waypoints.Count)
-            {
-                Destroy(gameObject);
-            }
         }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
